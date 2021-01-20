139ff5ecfe014c2a1c4e219692734dad9e139e8c
MaakTea <MaakTea@users.noreply.github.com>
2021-01-20 22:33:08 +0100

    Camera follow

--- Assets/Scripts/AiInput.cs
+++ Assets/Scripts/AiInput.cs
@@ -2,19 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AiInput : MonoBehaviour {
+public class AiInput : MonoBehaviour 
+{
 
     public Tank tankScript;
     public Turret turretScript;
 
+    public enum TargetType
+    {
+        Waypoint,
+        Enemy,
+        WasEnemy,
+        PotentialEnemy,
+        Powerup,
+        MAX
+    }
+    public enum ActionType
+    {
+        Attack,
+        MoveTo,     //investigate memory or patrol to waypoint, or go to powerup
+        Avoid,
+        Dodge,
+        Hide,       //?
+        MAX
+    }
+
+    [System.Serializable]
+    public class Target
+    {
+        public Vector3 lastKnownPos;
+        public TargetType type;
+        public Transform actualTarget;
+        public float timer;
+    }
+    [System.Serializable]
+    public class Action
+    {
+        public float cost;
+        public float benefit;
+        public ActionType type;
+    }
+
     public Transform target;
     public Transform[] targets;
+
+    public List<Target> currentTargets = new List<Target>();    //incl. "memory"
+    public List<Action> possibleActions = new List<Action>();
 
     //public float reloadSpeed;
     //public float reloadTimer = 5;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
@@ -52,6 +91,8 @@ public class AiInput : MonoBehaviour {
             turretScript.fireInput = true;
         }
 
+
+
         tankScript.steering = x;
         tankScript.gas = y;
 
@@ -60,4 +101,103 @@ public class AiInput : MonoBehaviour {
         turretScript.targetPos = target.position;
         turretScript.reloadSpeed = 5;
     }
+
+    public void FixedUpdate()
+    {
+        UpdateAI();
+    }
+
+    public void UpdateAI()
+    {
+        //ToDo:
+        // refresh targets (requires support code, "sensors")
+        UpdateMemory();
+        UpdateSensors();
+
+        //clear previous actions
+        possibleActions.Clear();
+        // maybe add a default action?
+
+        // loop through targets, and depending on its type and state, add potential actions to take about it (also calc. cost and benefit of action)
+        foreach(Target t in currentTargets)
+        {
+            switch(t.type)
+            {
+                case TargetType.Enemy:
+                    if (false)
+                    {
+                        Action a = new Action();
+                        a.type = ActionType.Attack;
+                        a.cost = 1;
+                        a.benefit = 1;
+                        //...
+                        possibleActions.Add(a);
+                    }
+                    //...
+                    break;
+            }
+        }
+        // pick best action (min cost, max benefit)
+        float bestValue = -1;
+        Action bestAction = null;
+        foreach(Action a in possibleActions)
+        {
+            float value = a.benefit - a.cost;
+            if (value > bestValue)
+            {
+                bestAction = a;
+                bestValue = value;
+            }
+        }
+
+        // execute 
+        if (bestAction != null)
+        {
+            switch(bestAction.type)
+            {
+                default:
+                    break;
+            }
+        }
+        else
+        {
+            //or do a default action here
+        }
+    }
+
+    public void UpdateMemory()
+    {
+        //ToDo: for each known target, check line of sight: can we still see it?
+        // if not, turn it into a memory
+        //for each memory, increase timer and if too old, forget it
+    }
+
+    public void UpdateSensors()
+    {
+        float viewDistance = 20.0f;
+        Vector3 dir = Quaternion.Euler(0, Random.Range(-180, 180), 0) * Vector3.forward;
+        Debug.DrawRay(transform.position + Vector3.up, dir * viewDistance, Color.black);
+        RaycastHit hit;
+        bool seen = Physics.Raycast(transform.position + Vector3.up, dir, out hit, viewDistance);
+        if (seen)
+        {
+            Tank tank = hit.collider.GetComponentInParent<Tank>();
+            if (tank != null)
+            {
+                //ToDo: check if we already know the same target
+                if (currentTargets.Find(ct => ct.actualTarget == tank.transform) != null)
+                { //we already have it
+                }
+                else
+                { //new!
+                    Target t = new Target();
+                    t.type = TargetType.Enemy;
+                    t.lastKnownPos = hit.collider.transform.position;
+                    t.actualTarget = tank.transform;
+                    currentTargets.Add(t);
+                }
+            }
+        }
+    }
+
 }
---
+++ Assets/Scripts/CameraRig.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRig : MonoBehaviour 
+{
+    public Transform target;
+    public float positionStrength = 0.1f;
+    public float rotationStrength = 0;
+
+	// Use this for initialization
+	void Start () 
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		if (target!=null)
+        {
+            transform.position = Vector3.Lerp(transform.position, target.position, positionStrength);
+            transform.rotation = Quaternion.Slerp(transform.rotation, target.rotation, rotationStrength);       //Slurm =o]
+        }
+	}
+}

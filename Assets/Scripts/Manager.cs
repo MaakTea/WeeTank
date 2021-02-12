f4a3342039fe0998d2295b598b5c0efbec18fb9e
MaakTea <MaakTea@users.noreply.github.com>
2021-02-12 22:03:42 +0100

    Menu, PvP, Manager

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -4,26 +4,52 @@ using UnityEngine;
 
 public class Manager : MonoBehaviour 
 {
-    public GameObject player;
-    public Health healthScript;
+    public static Manager singleton;
+
+    public List<GameObject> players;
 
     public int respawns = 0;
 
     public List<Transform> respawnPoints;
     public List<GameObject> tanks;
 
-    public UIManager uiManager;
-
     public bool autoPlay;
+
+    public bool loading;
+
+    void Awake()
+    {
+        if (singleton != null)
+        {
+            Destroy(this);
+            return;
+        }
+
+        singleton = this;
+        DontDestroyOnLoad(this.gameObject);
+
+        if (UIManager.singleton)
+            UIManager.singleton.Fade(0.0f);
+    }
 
 	// Use this for initialization
 	void Start () 
     {
+        Debug.Log("Start", this);
         Time.timeScale = 1;
+        
+        if (!loading)
+            if (UIManager.singleton)
+                UIManager.singleton.Fade(0.0f);
+
+
+        tanks.Clear();
+        players.Clear();
 
         Tank[] tank = FindObjectsOfType<Tank>();
         foreach(Tank t in tank)
         {
+            Debug.Log("tank ", t);
             tanks.Add(t.gameObject);
         }
 
@@ -32,6 +58,13 @@ public class Manager : MonoBehaviour
         //{
         //if (t.parent.tag == "Respawn") respawnPoints.Add(t);
         //}
+
+        PlayerInput[] p = FindObjectsOfType<PlayerInput>();
+        foreach(PlayerInput i in p) 
+        {
+            players.Add(i.gameObject);
+        }
+        autoPlay = players.Count > 0 ? false : true;
 
         if (autoPlay) 
         {
@@ -45,15 +78,18 @@ public class Manager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (loading) return;
+
         tanks.RemoveAll(t => t == null);
+        players.RemoveAll(t => t == null);
         
 		// -- our tank got destroyed
         // -- Restart the level
-        if (player == null && !autoPlay) 
+        if (players.Count == 0 && !autoPlay) 
         {
-            uiManager.EnableText(uiManager.lostText);
+            UIManager.singleton.EnableText(UIManager.singleton.lostText);
             Time.timeScale = 0.5f;
-            StartCoroutine(Wait(5, Application.loadedLevel));
+            StartCoroutine(Wait(3, Application.loadedLevel));
 			return;
         }
         
@@ -61,9 +97,9 @@ public class Manager : MonoBehaviour
         // -- Start the next level
         if(tanks.Count <= 1 && !autoPlay) 
         {
-            uiManager.EnableText(uiManager.victoryText);
+            UIManager.singleton.EnableText(UIManager.singleton.victoryText);
             Time.timeScale = 0.5f;
-            StartCoroutine(Wait(5, Application.loadedLevel+1));
+            StartCoroutine(Wait(3, Application.loadedLevel+1));
 			return;
         }
         if (autoPlay) 
@@ -86,8 +122,36 @@ public class Manager : MonoBehaviour
 
     IEnumerator Wait(int seconds, int levelIndex) 
     {
-        yield return new WaitForSecondsRealtime(seconds);
-        if (levelIndex != Application.levelCount) Application.LoadLevel(levelIndex);
-        else Application.LoadLevel(0);
+        loading = true;
+
+        //yield return new WaitForSecondsRealtime(seconds);
+        for(float f=0; f<=1.0f; f+=0.1f/seconds)
+        {
+            yield return new WaitForSecondsRealtime(0.1f);
+            UIManager.singleton.Fade(f);
+        }
+
+        if (levelIndex != Application.levelCount) 
+            Application.LoadLevel(levelIndex);
+        else
+            Application.LoadLevel(0);
+
+        yield return new WaitForEndOfFrame();
+        yield return new WaitForFixedUpdate();
+        Start();
+
+        for (float f = 0; f <= 1.0f; f += 0.1f / seconds)
+        {
+            yield return new WaitForSecondsRealtime(0.1f);
+            UIManager.singleton.Fade(1.0f-f);
+        }
+
+        loading = false;
+    }
+
+    public static void LoadLevel(int levelindex)
+    {
+        //Application.LoadLevel(levelindex);
+        singleton.StartCoroutine(singleton.Wait(2, levelindex));
     }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UIManager : MonoBehaviour {
+public class UIManager : MonoBehaviour 
+{
+    public static UIManager singleton;
+
 
     public Text victoryText;
     public Text lostText;
 
-	// Use this for initialization
-	void Start () 
-    {
+    public RawImage fadeScreen;
 
+	// Use this for initialization
+	void Awake() 
+    {
+        //if (singleton != null)
+        //{
+        //    Debug.LogWarning("Multiple UIManagers!", this);
+        //    Destroy(this);
+        //}
+
+        singleton = this;
 	}
 	
 	// Update is called once per frame
@@ -23,4 +34,16 @@ public class UIManager : MonoBehaviour {
     {
         text.gameObject.SetActive(true);
     }
+
+    public void Fade(float f)
+    {
+        if (fadeScreen)
+            fadeScreen.color = new Color(0, 0, 0, f);
+    }
+
+    public void LoadLevel(int levelindex)
+    {
+        Manager.LoadLevel(levelindex);
+    }
+
 }
